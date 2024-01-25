24bc8b0e438a9c0b10294cd062b726b7d95605e5
Home <Home@MacBook-Pro-2.local>
2024-01-25 14:01:33 -0800

    cleanup

--- Components/SVG/IconWrapper.svelte
+++ Components/SVG/IconWrapper.svelte
@@ -1,13 +1,8 @@
 <script>
-	import Icon, { directions } from "./Icon.svelte";
+	import Icon from "./Icon.svelte";
 
 	export let direction = "s";
 	export let option = "chevronDown";
-	// const onRotate = () => {
-	// 	directionIndex = (directionIndex + 1) % directions.length;
-	// };
 </script>
 
-<!-- <button on:click={onRotate}> -->
 <Icon name={option} {direction} />
-<!-- </button> -->
